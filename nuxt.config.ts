a8c95227b584c0403b66ac992df85ce2cce358b0
Sahil Patel <smppatel999@gmail.com>
2022-12-28 07:47:02 +0530

    Add env var to nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    runtimeConfig: {
+        // Private keys are only available on the server
+        privateVar: process.env.NUXT3_PRIVATE_VAR,
+        // Public keys that are exposed to the client
+        public: {
+            publicVar: process.env.NUXT3_PUBLIC_VAR
+        }
+      }
 })
